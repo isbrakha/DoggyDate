a1d55f8687ba172b9875f65d7720de5d4c711f66
brakhais <isaacbrakha14@gmail.com>
2023-11-22 18:36:32 -0500

    dogs route added for create and new

--- starter-app/routes/dogs.js
+++ starter-app/routes/dogs.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dogsCtrl = require("../controllers/dogs")
 
-router.get('/new', dogsCtrl.new)
+router.get('owners/:id/dogs/new', dogsCtrl.new)
+router.post('owners/:id/dogs', dogsCtrl.create)
 
 module.exports = router;
--- starter-app/server.js
+++ starter-app/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/owners', ownersRouter);
-app.use('/dogs', dogsRouter)
+app.use('/', dogsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
