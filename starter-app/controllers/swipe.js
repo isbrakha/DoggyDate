094f3d2e68a9e9920bd73125f9b85985721b7941
Eraclio Vega <eracliovega@Eraclios-MBP.attlocal.net>
2023-11-29 14:32:52 -0500

    test

--- starter-app/controllers/swipe.js
+++ starter-app/controllers/swipe.js
@@ -9,68 +9,6 @@ module.exports = {
 }
 
 
-// async function nextDog(dogId){
-//   const currentDog = await Dog.findById(dogId);
-//   if (!currentDog) {
-//     console.log(`Dog with ID ${dogId} not found`);
-//     return null;
-//   }
-//   const dogs = await Dog.find({
-//     _id: {$ne: dogId},
-//     _id: {$nin: currentDog.likes},
-//     _id: {$nin: currentDog.dislikes},
-//   });
-
-
-//   const rndmInt = Math.floor(Math.random() * dogs.length)
-//   return dogs[rndmInt];
-// }
-
-// async function like(req,res){
-//   const dogId = req.session.swipingDogId;
-//   const likedDogId = req.params.otherDogId;
-
-//   try{
-//     console.log('dogId = ' + dogId)
-//     console.log('likedDogId = ' + likedDogId)
-//     await Dog.findByIdAndUpdate(dogId, {$addToSet:{ likes: likedDogId }});
-//     const owner = await Owner.findById(req.params.ownerId)
-//     const likedDog = await Dog.findById(likedDogId);
-//     if(likedDog.likes.includes(dogId)) {
-//       // something ewhen match
-//     }
-//     const showNextDog = await nextDog(dogId);
-//     if(nextDog){
-//       res.redirect(`/owners/${owner._id}/dogs/${showNextDog._id}/swipe`)
-//     } else{
-//       res.redirect('no')
-//     }
-
-//   } catch (err){
-//     console.log(err);
-//     res.status(500)
-//   }
-// }
-
-// async function dislike(req,res){
-//   const dogId = req.session.swipingDogId;
-//   const dislikedDogId = req.params.otherDogId;
-
-//   try{
-//     await Dog.findByIdAndUpdate(dogId, {$addToSet:{ dislikes: dislikedDogId }});
-//     const nextDog = await nextDog(dogId);
-//     if(nextDog){
-//       res.redirect('/swipe/${nextDog._id');
-//     } else{
-//       res.redirect('no')
-//     }
-
-//   } catch (err){
-//     console.log(err);
-//     res.status(500)
-//   }
-// }
-
 async function startSwiping (req, res) {
   try {
     const userDog = await Dog.findById(req.params.dogId)
